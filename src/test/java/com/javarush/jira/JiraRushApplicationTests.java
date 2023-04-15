48a456e0ab0e4a1b86ee17866f0fa6d9f616386b
sergey <drogunov-s@yandex.ru>
2023-04-15 22:28:11 +0300

    WIT Save configuration DataSource in test on feature

--- src/test/java/com/javarush/jira/JiraRushApplicationTests.java
+++ src/test/java/com/javarush/jira/JiraRushApplicationTests.java
@@ -1,13 +1,42 @@
 package com.javarush.jira;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JiraRushApplicationTests {
-	@Disabled("Need refactoring")
-	@Test
-	void contextLoads() {
-	}
+
+    @Test
+    void contextLoads() {
+    }/*
+
+    @Container
+    public static PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer(
+            DockerImageName.parse("postgres:15.2")
+                    .asCompatibleSubstituteFor("postgresTestcontainers")
+    )
+            .withDatabaseName("db")
+            .withUsername("test_user")
+            .withPassword("test_password");
+
+    @BeforeAll
+    public static void setup() {
+        postgresqlContainer.setWaitStrategy(
+                new LogMessageWaitStrategy()
+                        .withRegEx(".*database system is ready to accept connections.*\\s")
+                        .withTimes(1)
+                        .withStartupTimeout(Duration.of(60, ChronoUnit.SECONDS))
+        );
+        postgresqlContainer.start();
+    }
+
+    @DynamicPropertySource
+    public static void overrideProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", postgresqlContainer::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", postgresqlContainer::getPassword);
+        dynamicPropertyRegistry.add("spring.datasource.driver-class-name", postgresqlContainer::getDriverClassName);
+    }*/
 }
