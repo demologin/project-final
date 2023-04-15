6710dff0cb280df1899007d1da4634dfa0a432a0
sergey <drogunov-s@yandex.ru>
2023-04-15 23:23:45 +0300

    Complete task 4

--- src/test/java/com/javarush/jira/JiraRushApplicationTests.java
+++ src/test/java/com/javarush/jira/JiraRushApplicationTests.java
@@ -10,33 +10,5 @@ class JiraRushApplicationTests {
 
     @Test
     void contextLoads() {
-    }/*
-
-    @Container
-    public static PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer(
-            DockerImageName.parse("postgres:15.2")
-                    .asCompatibleSubstituteFor("postgresTestcontainers")
-    )
-            .withDatabaseName("db")
-            .withUsername("test_user")
-            .withPassword("test_password");
-
-    @BeforeAll
-    public static void setup() {
-        postgresqlContainer.setWaitStrategy(
-                new LogMessageWaitStrategy()
-                        .withRegEx(".*database system is ready to accept connections.*\\s")
-                        .withTimes(1)
-                        .withStartupTimeout(Duration.of(60, ChronoUnit.SECONDS))
-        );
-        postgresqlContainer.start();
     }
-
-    @DynamicPropertySource
-    public static void overrideProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
-        dynamicPropertyRegistry.add("spring.datasource.url", postgresqlContainer::getJdbcUrl);
-        dynamicPropertyRegistry.add("spring.datasource.username", postgresqlContainer::getUsername);
-        dynamicPropertyRegistry.add("spring.datasource.password", postgresqlContainer::getPassword);
-        dynamicPropertyRegistry.add("spring.datasource.driver-class-name", postgresqlContainer::getDriverClassName);
-    }*/
 }
