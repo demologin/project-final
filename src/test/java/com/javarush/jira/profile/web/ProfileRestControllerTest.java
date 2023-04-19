882e839d5e26393b1cc35b7f4a0e8119540dd10d
etherpoint <danila02478@gmail.com>
2023-04-20 00:42:49 +0300

    refactoring test

--- src/test/java/com/javarush/jira/profile/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/web/ProfileRestControllerTest.java
@@ -1,25 +1,50 @@
 package com.javarush.jira.profile.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.common.config.PGContainer;
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static com.javarush.jira.login.internal.web.UserTestData.ADMIN_MAIL;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class ProfileRestControllerTest extends AbstractControllerTest {
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer;
 
-    /**
-     * Так как метод get() класса ProfileRestController состоит только из реализации super.get(),
-     * то будет целеобразно реализовать unit тестирование именно в родительском классе
-     */
-    @Test
-    void stubMethodGet() {
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        postgreSQLContainer = PGContainer.getInstance();
+        postgreSQLContainer.start();
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
     }
 
-    /**
-     * Так как метод update() класса ProfileRestController состоит только из реализации super.update(),
-     * то будет целеобразно реализовать unit тестирование именно в родительском классе
-     */
+    private static final String REST_URL = "/api/profile";
+
     @Test
-    void stubMethodUpdate() {
+    @WithUserDetails(ADMIN_MAIL)
+    void stubMethodGet() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    @WithUserDetails(ADMIN_MAIL)
+    void stubMethodUpdate(){
+
     }
 }
