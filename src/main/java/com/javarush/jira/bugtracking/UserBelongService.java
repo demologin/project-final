bf60765ac2224caa8a26829c027303ff51a6c58b
etherpoint <danila02478@gmail.com>
2023-04-23 20:54:19 +0300

    added methods for subscribing to a task (task 7 completed)

---
+++ src/main/java/com/javarush/jira/bugtracking/UserBelongService.java
@@ -0,0 +1,33 @@
+package com.javarush.jira.bugtracking;
+
+import com.javarush.jira.bugtracking.internal.model.UserBelong;
+import com.javarush.jira.bugtracking.internal.repository.UserBelongRepository;
+import com.javarush.jira.bugtracking.to.ObjectType;
+import com.javarush.jira.login.Role;
+import com.javarush.jira.login.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserBelongService {
+    private final UserBelongRepository belongRepository;
+
+    public UserBelongService(UserBelongRepository repository) {
+        this.belongRepository = repository;
+    }
+
+    //todo added method for subscribing to a task
+    public void subscribeToTask(User user, Long taskId) {
+        UserBelong userBelong = new UserBelong();
+        userBelong.setUserId(user.id());
+        if (user.hasRole(Role.ADMIN)) {
+            userBelong.setUserTypeCode("admin");
+        } else if (user.hasRole(Role.DEV)) {
+            userBelong.setUserTypeCode("dev");
+        } else {
+            userBelong.setUserTypeCode("quest");
+        }
+        userBelong.setObjectId(taskId);
+        userBelong.setObjectType(ObjectType.TASK);
+        belongRepository.save(userBelong);
+    }
+}
