94acd522bf622adc5ea88ff5da33b045d0e7379e
Mikhail <mzazimko@icloud.com>
2023-04-25 17:46:04 +0300

    make test ProfileRestControllerTest.java

--- src/test/java/com/javarush/jira/profile/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/web/ProfileRestControllerTest.java
@@ -3,7 +3,6 @@ package com.javarush.jira.profile.web;
 import com.javarush.jira.AbstractControllerTest;
 import com.javarush.jira.common.config.Initializerpostgres;
 import com.javarush.jira.profile.ProfileTo;
-import com.javarush.jira.profile.internal.Profile;
 import com.javarush.jira.profile.internal.ProfileMapper;
 import com.javarush.jira.profile.internal.ProfileRepository;
 import org.junit.jupiter.api.BeforeAll;
@@ -15,45 +14,42 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static com.javarush.jira.login.internal.web.UserTestData.USER_MAIL;
 import static com.javarush.jira.profile.web.ProfileTestData.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ProfileRestControllerTest  extends AbstractControllerTest {
+
     @Autowired
     protected ProfileMapper profileMapper;
-    @Autowired
-    private ProfileRepository profileRepository;
+
     @BeforeAll
     static void init(){
         Initializerpostgres.postgreSQLContainer.start();
     }
 
-//    @Test
-//    void get() throws Exception {
-//        perform(MockMvcRequestBuilders.get(ProfileRestController.REST_URL))
-//                //.andExpect(status().isOk())
-//                .andDo(print())
-//                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-//                .andExpect(PROFILE_MATCHER.contentJson(profile));
-//    }
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void get() throws Exception {
+     perform(MockMvcRequestBuilders.get("/api/profile"))
+             .andExpect(status().isOk())
+             .andDo(print())
+             .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
+             .andExpect(PROFILE_MATCHER.contentJson(profileTo));
+    }
 
-//    @Test
-//    void update() throws Exception {
-//        Profile profileBefore = profileRepository.getExisted(PROFILE_ID);
-//        ProfileTo updatedTo = profileMapper.toTo(getUpdated());
-//        updatedTo.setId(999L);
-//        perform(MockMvcRequestBuilders.put(ProfileRestController.REST_URL)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(jsonWithPassword(updatedTo, updatedTo.getId())))
-//                .andDo(print());
-//             //   .andExpect(status().isNoContent());
-//
-//        Profile profileAfter = profileRepository.getExisted(PROFILE_ID);
-//        assertEquals(profileBefore.getId(),updatedTo.getId(),"profile's id must not be changed");
-//        Profile updated = getUpdated();
-//        updated.setMailNotifications(profileBefore.getMailNotifications());
-//        PROFILE_MATCHER.assertMatch(profileBefore,updated);
-//    }
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void update() throws Exception {
+        ProfileTo updateTo = getUpdated();
+        updateTo.setId(null);
+        perform(MockMvcRequestBuilders.put("/api/profile")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(jsonWithId(updateTo,String.valueOf(updateTo.getId()))))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        ProfileTo updatedTo2 = getUpdated();
+        updateTo.setId(updatedTo2.getId());
+        PROFILE_MATCHER.assertMatch(updateTo,updatedTo2);
+    }
 }
--- src/test/java/com/javarush/jira/profile/web/ProfileTestData.java
+++ src/test/java/com/javarush/jira/profile/web/ProfileTestData.java
@@ -1,27 +1,31 @@
 package com.javarush.jira.profile.web;
-
 import com.javarush.jira.MatcherFactory;
 import com.javarush.jira.common.util.JsonUtil;
+import com.javarush.jira.profile.ContactTo;
 import com.javarush.jira.profile.ProfileTo;
 import com.javarush.jira.profile.internal.Profile;
 
+import java.util.Set;
+
 public class ProfileTestData {
-    public static final MatcherFactory.Matcher<Profile> PROFILE_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(
-            Profile.class, "lastLogin", "lastFailedLogin", "mailNotifications");
+    public static final MatcherFactory.Matcher<ProfileTo> PROFILE_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(
+            ProfileTo.class, "lastLogin", "mailNotifications", "contacts");
 
-    public static final MatcherFactory.Matcher<ProfileTo> TO_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(ProfileTo.class);
-
-    public static final long PROFILE_ID = 1L;
-
-    public static final Profile profile = new Profile(PROFILE_ID);
+     public static final ProfileTo profileTo = new ProfileTo(1L, Set.of("deadline", "overdue", "assigned"),Set.of(new ContactTo("mobile", "+01234567890"),
+            new ContactTo("website", "user.com"), new ContactTo("skype", "userSkype")));
 
 
     public static Profile getNew() {
         return new Profile();
     }
 
-    public static Profile getUpdated() {
-        return new Profile(PROFILE_ID);
+    public static ProfileTo getUpdated() {
+        return new ProfileTo(1L, Set.of("deadline", "overdue", "assigned"),
+                Set.of(new ContactTo("mobile", "+01234567890"),
+                new ContactTo("website", "user.com"), new ContactTo("skype", "userSkype")));
+    }
+    public static <T> String jsonWithId(T user, String id) {
+        return JsonUtil.writeAdditionProps(user, "Id", id);
     }
 
 }
