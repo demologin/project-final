509d7ce1d123e50533ec45938a9fe79fb9c5e8da
Dmytro Shubchynskyi <shubchinskiydmitriy@gmail.com>
2023-07-11 14:08:51 +0200

    p4 ready (test containers)

--- src/test/java/com/javarush/jira/BaseTests.java
+++ src/test/java/com/javarush/jira/BaseTests.java
@@ -1,9 +1,23 @@
 package com.javarush.jira;
 
+import com.javarush.jira.initializer.PostgresTestContainersInitializer;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 
+// todo task 4
 @SpringBootTest
 @ActiveProfiles("test")
+@ContextConfiguration(initializers = {
+        PostgresTestContainersInitializer.Initializer.class
+})
 abstract class BaseTests {
+
+    @BeforeAll
+    static void init(){
+        PostgresTestContainersInitializer.container.start();
+    }
 }
