0b3496a24f104f9431bd8292fdc7d71ab364a133
Igor Dobrov <idob230491@gmail.com>
2023-07-14 17:51:29 +1000

    3 done diff

--- src/main/java/com/javarush/jira/common/internal/config/AppConfig.java
+++ src/main/java/com/javarush/jira/common/internal/config/AppConfig.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import com.javarush.jira.common.util.JsonUtil;
+import jakarta.annotation.sql.DataSourceDefinition;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
@@ -18,6 +21,7 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import javax.sql.DataSource;
 import java.util.Map;
 import java.util.concurrent.Executor;
 
@@ -43,6 +47,26 @@ public class AppConfig {
                 setThreadNamePrefix("mail-");
             }
         };
+    }
+    @Bean
+    @Profile("prod")
+    public DataSource postgresDataSource(){
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5432/jira")
+                .driverClassName("org.postgresql.Driver")
+                .username("jira")
+                .password("JiraRush")
+                .build();
+    }
+    @Bean
+    @Profile("test")
+    public DataSource postgresTestDataSource(){
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5432/jira")
+                .driverClassName("org.postgresql.Driver")
+                .username("jira")
+                .password("JiraRush")
+                .build();
     }
 
     public boolean isProd() {
