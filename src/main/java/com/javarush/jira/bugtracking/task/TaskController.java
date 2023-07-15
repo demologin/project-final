39251ed609989f7f345baefd155f5964ac4190ef
bulo4kasmyasom <benetoru@gmail.com>
2023-07-15 13:18:20 +0300

    added methods for create task tags and update items in readme

--- src/main/java/com/javarush/jira/bugtracking/task/TaskController.java
+++ src/main/java/com/javarush/jira/bugtracking/task/TaskController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import static com.javarush.jira.common.BaseHandler.createdResponse;
 
@@ -45,6 +46,13 @@ public class TaskController {
     public TaskToFull get(@PathVariable long id) {
         log.info("get task by id={}", id);
         return taskService.get(id);
+    }
+
+    @PostMapping("/tag/{id}")
+    public void createTaskTag(@PathVariable("id") Long taskId, @RequestBody Set<String> tags)
+    {
+        log.info("create task tags for task with id={}", taskId);
+        taskService.createTaskTag(taskId, tags);
     }
 
     @GetMapping("/by-sprint")
