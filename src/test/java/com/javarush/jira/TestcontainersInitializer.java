aea34841ce768014fa6dabdfe072b4aef46f23b8
oleksandrosypenko <Oleksandrosypenko1989@gmail.com>
2023-07-19 12:39:40 +0200

    task 4 completed

---
+++ src/test/java/com/javarush/jira/TestcontainersInitializer.java
@@ -0,0 +1,28 @@
+package com.javarush.jira;
+
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.lifecycle.Startables;
+import org.testcontainers.utility.DockerImageName;
+
+public class TestcontainersInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    static PostgreSQLContainer<?> database = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+
+    static {
+        database.start();
+//        Startables.deepStart(database).join();
+    }
+
+    @Override
+    public void initialize(ConfigurableApplicationContext ctx) {
+        TestPropertyValues.of(
+                "spring.datasource.url=" + database.getJdbcUrl(),
+                "spring.datasource.username=" + database.getUsername(),
+                "spring.datasource.password=" + database.getPassword()
+        ).applyTo(ctx.getEnvironment());
+    }
+}
