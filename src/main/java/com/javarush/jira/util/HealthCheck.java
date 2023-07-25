9b1fa713298a6fd96ae09018ca7934232e8c9209
Dmytro Shubchynskyi <shubchinskiydmitriy@gmail.com>
2023-07-25 16:19:38 +0200

    HealthCheck for docker-compose updated

--- src/main/java/com/javarush/jira/util/HealthCheck.java
+++ src/main/java/com/javarush/jira/util/HealthCheck.java
@@ -7,35 +7,45 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
 //TODO p10 (docker-compose). For health check
 @UtilityClass
 public class HealthCheck {
-    private static final HttpClient client = HttpClient.newHttpClient();
+    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+    private static final String SERVER_URL = "http://localhost:8080/view/login";
     private static final int MAX_RETRIES = 10;
+    private static final int WAIT_TIME = 5000;
+    private static final int HTTP_OK = 200;
+    private static final String SERVER_UP_MSG = "Server is up and running!";
+    private static final String SERVER_NOT_READY_MSG = "Attempt %d failed. Server is not ready yet. Waiting...";
+    private static final String SERVER_NOT_RESPONDING_MSG = "Server is not responding after %d attempts.";
 
-    public static void main(String[] args) throws InterruptedException {
-        String serverUrl = "http://localhost:8080/view/login";
+    public static void main(String[] args) {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(serverUrl))
+                .uri(URI.create(SERVER_URL))
                 .build();
 
-        int retries = 0;
-        while (retries < MAX_RETRIES) {
+        tryConnecting(request);
+    }
+
+    private static void tryConnecting(HttpRequest request) {
+        for (int retries = 0; retries < MAX_RETRIES; retries++) {
             try {
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                if (response.statusCode() == 200) {
-                    System.out.println("Server is up and running!");
-                    break;
+                HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() == HTTP_OK) {
+                    System.err.println(SERVER_UP_MSG);
+                    System.exit(0);
                 }
             } catch (Exception e) {
-                System.out.println("Server is not ready yet. Waiting...");
-                Thread.sleep(5000); // Wait 5 seconds before trying again
+                System.out.printf((SERVER_NOT_READY_MSG) + "%n", retries + 1);
+                try {
+                    Thread.sleep(WAIT_TIME);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                }
             }
-            retries++;
         }
-
-        if (retries == MAX_RETRIES) {
-            System.out.println("Server is not responding after " + MAX_RETRIES + " attempts.");
-        }
+        System.err.printf((SERVER_NOT_RESPONDING_MSG) + "%n", MAX_RETRIES);
+        System.exit(1);
     }
 }
