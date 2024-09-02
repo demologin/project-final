2c9187dd5a130a8016f25f3b75f7f833eab2f3f7
Evgeny Alpeyev <krotpk@yandex.ru>
2024-09-02 23:37:48 +0300

    Refactor method of "com.javarush.jira.bugtracking.attachment.FileUtil#upload". It must be used modern approach for working with files.

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -20,15 +20,25 @@ import java.nio.file.Paths;
 public class FileUtil {
     private static final String ATTACHMENT_PATH = "./attachments/%s/";
 
-    public static void upload(MultipartFile multipartFile, String directoryPath, String fileName) {
+    public static void upload(MultipartFile multipartFile, String directoryPath, String fileName)  {
         if (multipartFile.isEmpty()) {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
 
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
+        Path dir = null;
+        try {
+           dir = Files.createDirectories(Paths.get(directoryPath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (Files.exists(dir)) {
+            Path file = null;
+            try {
+                file = Files.createFile(Paths.get(directoryPath + fileName));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try (OutputStream outStream = new FileOutputStream(file.toFile())) {
                 outStream.write(multipartFile.getBytes());
             } catch (IOException ex) {
                 throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
