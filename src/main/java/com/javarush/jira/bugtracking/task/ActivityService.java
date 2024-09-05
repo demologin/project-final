2bc54e4a1bb8752c582246c8abcb2bd9d79ae221
Said-Khasanov <said.hasanow@yandex.ru>
2024-09-05 16:44:30 +0400

    Completed task 8. ADDED methods for calculate time spent on work and test

--- src/main/java/com/javarush/jira/bugtracking/task/ActivityService.java
+++ src/main/java/com/javarush/jira/bugtracking/task/ActivityService.java
@@ -3,11 +3,14 @@ package com.javarush.jira.bugtracking.task;
 import com.javarush.jira.bugtracking.Handlers;
 import com.javarush.jira.bugtracking.task.to.ActivityTo;
 import com.javarush.jira.common.error.DataConflictException;
+import com.javarush.jira.common.error.NotFoundException;
 import com.javarush.jira.login.AuthUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.javarush.jira.bugtracking.task.TaskUtil.getLatestValue;
@@ -15,9 +18,14 @@ import static com.javarush.jira.bugtracking.task.TaskUtil.getLatestValue;
 @Service
 @RequiredArgsConstructor
 public class ActivityService {
+    public static final String STATUS_READY_FOR_REVIEW = "ready_for_review";
+    public static final String STATUS_IN_PROGRESS = "in_progress";
+    public static final String STATUS_DONE = "done";
+    public static final String TIME_WITH_DAYS_FORMAT = "%d d %d h %d m %d s";
     private final TaskRepository taskRepository;
 
     private final Handlers.ActivityHandler handler;
+    private final ActivityRepository activityRepository;
 
     private static void checkBelong(HasAuthorId activity) {
         if (activity.getAuthorId() != AuthUser.authId()) {
@@ -72,5 +80,36 @@ public class ActivityService {
                 task.setTypeCode(latestType);
             }
         }
+    }
+
+    public String getTimeSpentOnWork(long taskId) {
+        return getTimeBetweenStatus(taskId, STATUS_IN_PROGRESS, STATUS_READY_FOR_REVIEW);
+    }
+
+    public String getTimeSpentOnTest(long taskId) {
+        return getTimeBetweenStatus(taskId, STATUS_READY_FOR_REVIEW, STATUS_DONE);
+    }
+
+    private String getTimeBetweenStatus(long taskId, String startStatus, String endStatus) {
+        taskRepository.getExisted(taskId);
+        LocalDateTime start = getActivityUpdatedByTaskIdAndStatus(taskId, startStatus);
+        LocalDateTime end = getActivityUpdatedByTaskIdAndStatus(taskId, endStatus);
+        Duration duration = Duration.between(start, end);
+        return TIME_WITH_DAYS_FORMAT.formatted(duration.toDaysPart(),
+                duration.toHoursPart(), duration.toMinutesPart(), duration.toSecondsPart()
+        );
+    }
+
+    private LocalDateTime getActivityUpdatedByTaskIdAndStatus(long taskId, String status) {
+        return activityRepository
+                .findAllByTaskIdOrderByUpdatedDesc(taskId)
+                .stream()
+                .filter(activity -> activity.getUpdated() != null)
+                .filter(activity -> status.equals(activity.getStatusCode()))
+                .findFirst()
+                .orElseThrow(
+                        () -> new NotFoundException("Task " + taskId + " doesn't have activity with status "+ status)
+                )
+                .getUpdated();
     }
 }
