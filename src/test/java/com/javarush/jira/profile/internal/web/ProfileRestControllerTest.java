f87663d56f3009f4818a831f64549905da2e53d4
Evgeny Alpeyev <krotpk@yandex.ru>
2024-09-12 14:23:55 +0300

    Add tests for ProfileRestController

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,89 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.common.util.JsonUtil;
+import com.javarush.jira.profile.ContactTo;
+import com.javarush.jira.profile.ProfileTo;
+import com.javarush.jira.profile.internal.ProfileMapper;
+import com.javarush.jira.profile.internal.model.Contact;
+import com.javarush.jira.profile.internal.ProfileMapperImpl;
+import com.javarush.jira.profile.internal.ProfileRepository;
+
+import com.javarush.jira.profile.internal.model.Profile;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+import static com.javarush.jira.login.internal.web.UserTestData.*;
+import static com.javarush.jira.profile.internal.web.ProfileRestController.REST_URL;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 class ProfileRestControllerTest extends AbstractControllerTest {
+    @Autowired
+    private ProfileRepository profileRepository;
+    @Autowired
+    private ProfileMapper  profileMapper;
+    @Autowired
+    private ProfileMapperImpl profileMapperImpl;
+    @Test
+    void getUnautorized() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getAuthorisedUserProfile() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_MATCHER_TO.contentJson(USER_PROFILE_TO));
+    }
+    @Test
+    @WithUserDetails(value = GUEST_MAIL)
+    void getGuestEmptyProfile() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_MATCHER_TO.contentJson(GUEST_PROFILE_EMPTY_TO));
+    }
+
+    @Test
+    void updateUnautorized() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getUpdatedTo())))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateWhithInvalidTo() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getInvalidTo())))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    @Transactional
+    void checkUpdateDBUserProfile() throws Exception {
+        Profile profile = profileRepository.getExisted(USER_ID);
+        ProfileTo profileTo = profileMapper.toTo(profile);
+        profileTo.setContacts(getContactsTo());
+        Profile updatedProfile = profileMapper.updateFromTo(profileRepository.getExisted(USER_ID),
+                                                            profileTo);
+        PROFILE_MATCHER.assertMatch(updatedProfile, profile);
+    }
 
 }
