34ffc929a3f76ac042e352099e143affdc3ad6bd
nikolenolga <nikolenolga@gmail.com>
2024-11-30 11:25:58 +0300

    add TestContainer, change jira-test db to test containers - task 4

--- src/test/java/com/javarush/jira/BaseTests.java
+++ src/test/java/com/javarush/jira/BaseTests.java
@@ -1,9 +1,29 @@
 package com.javarush.jira;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
-@SpringBootTest
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 abstract class BaseTests {
+    private static final String IMAGE_VERSION = "postgres:latest";
+
+    static final PostgreSQLContainer<?> CONTAINER = new PostgreSQLContainer<>(IMAGE_VERSION);
+
+    static {
+        CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void datasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.password", CONTAINER::getPassword);
+        registry.add("spring.datasource.username", CONTAINER::getUsername);
+    }
+
 }
